14df7dbf798a211a6e2e32bfb6e4b23d1ea51640
tuanduog <tuanduog04@gmail.com>
2025-05-10 14:36:28 +0700

    init

---
+++ src/main/java/com/edu/student_management_backend/StudentManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.edu.student_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/student_management_backend/StudentManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.student_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
